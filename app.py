86836412fca34a15945397a9408c67df2d4990a4
ChungHaLee <ssk04040@naver.com>
2022-05-26 13:53:27 +0900

    edited emotional words

--- app.py
+++ app.py
@@ -12,32 +12,29 @@ def index():
 @app.route('/result', methods=['POST'])
 def result():
   if request.method == 'POST':
-    if request.form['word_type'] == 'dynamic':
-      word = '다이나믹한'
+    if request.form['word_type'] == 'amazement':
+      word = 'Amazement'
       haptic = haptic_function()  # 감정 단어에 맞게 햅틱 함수를 다르게 하면 될듯. haptic_dynamic, haptic_sad 이런식으로...
-    elif request.form['word_type'] == 'energetic':
-      word = '에너제틱한'
+    elif request.form['word_type'] == 'tenderness':
+      word = 'Tenderness'
       haptic = haptic_function()
-    elif request.form['word_type'] == 'catchy':
-      word = '시선을 확 끄는'
+    elif request.form['word_type'] == 'nostalgia':
+      word = 'Nostalgia'
       haptic = haptic_function()
-    elif request.form['word_type'] == 'relaxed':
-      word = '차분한'
+    elif request.form['word_type'] == 'calmness':
+      word = 'Calmness'
       haptic = haptic_function()
-    elif request.form['word_type'] == 'bored':
-      word = '지루한'
+    elif request.form['word_type'] == 'power':
+      word = 'Power'
       haptic = haptic_function()
-    elif request.form['word_type'] == 'fear':
-      word = '두려운'
+    elif request.form['word_type'] == 'joyful':
+      word = 'Joyful'
       haptic = haptic_function()
-    elif request.form['word_type'] == 'sad':
-      word = '슬픈'
+    elif request.form['word_type'] == 'tension':
+      word = 'Tension'
       haptic = haptic_function()
-    elif request.form['word_type'] == 'angry':
-      word = '화나는'
-      haptic = haptic_function()
-    elif request.form['word_type'] == 'annoyed':
-      word = '짜증나는'
+    elif request.form['word_type'] == 'sadness':
+      word = 'Sadness'
       haptic = haptic_function()
     else:
       pass
