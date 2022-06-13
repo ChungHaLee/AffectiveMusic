1677cbfe3f50028035642b1598cb17b11dc597d7
songmin <85054482+thdals3407@users.noreply.github.com>
2022-06-13 14:36:10 +0900

    Add files via upload

---
+++ Music_Json_file.py
@@ -0,0 +1,57 @@
+import numpy as np
+import librosa
+
+def Music_Name_To_Path(Music_array):    #음악 이름 array를 path로 바꿔주는 함수입니다. 음악 파일, 확장자명이 변경되면 수정이 필요
+    path = "music/"
+    file_type = ".mp3"
+    for i in range(len(Music_array)):
+        Music_array[i] = path + Music_array[i] + file_type
+    return Music_array
+
+
+def amplitude_maker(file, fps):     #fps 값으로 Amplitude haptic pattern으로 변형하는 함수
+    sig, _ = librosa.load(file, sr=22050)
+    plat_num = int(22050/fps)       #Fps가 높아도 loop 속도에 맞춰서 출력되기 때문에 높아도 상관은 없지만 무거워지니 일단 10으로
+    sig_1 = np.zeros(int(len(sig) / plat_num))
+    for i_1 in range(len(sig_1)):
+        sig_1[i_1] = abs(sig[i_1 * plat_num])
+    sig_1 = sig_1 / np.max(sig_1) * 255
+    return sig_1.astype(np.int64)
+
+
+
+# 감정 키워드 함수입니다. 늘어나면 같이 추가하면 됩니다.
+Emotion_array = ["Amazement",
+                 "Tenderness",
+                 "Nostalgia",
+                 "Calmness",
+                 "Power",
+                 "Jpyful",
+                 "Tension",
+                 "Sadness"]
+
+# 음악 이름입니다, 만약 haptic array 저장 코드가 주석으로 되어 있으면 음악 이름 변경 후, 밑의 코드를 실행해야합니다.
+Music_array   = ["amazement",
+                 "tenderness",
+                 "nostalgia",
+                 "calmness",
+                 "power",
+                 "joyful_activation",
+                 "tension",
+                 "sadness"]
+
+# 간단히 array를 가져올 수 있도록 변형하는 코드입니다.
+Music_array = Music_Name_To_Path(Music_array)
+Emotion_array = np.array(Emotion_array)
+Music_array = np.array(Music_array)
+
+# 음악을 햅틱으로 변형하여 저장하는 코드입니다.
+# 음악이 바뀌면 다시 haptic pattern을 저장해야하기 때문에 꼭 한번은 실행해야합니다.
+# 주석처리를 안해도 main 코드 실행에는 문제가 없지만, main 실행에 memory랑 시간 낭비가 심할 수 있습니다.
+
+"""haptic_array = []
+for i in Music_array:
+    haptic_array.append(amplitude_maker(i, 10))
+haptic_array = np.array(haptic_array)
+np.save("haptic_array", haptic_array)
+"""
---
+++ main.py
@@ -0,0 +1,181 @@
+import asyncio
+from bleak import BleakClient
+from bleak import discover
+from neosensory_python import NeoDevice
+import numpy as np
+import time
+from Music_Json_file import Emotion_array, Music_array
+import librosa
+import pygame
+from flask import Flask, render_template, request
+from multiprocessing import Process, Manager
+
+
+# 정하가 만든 html 코드입니다. 따로 수정은 안했지만, haptic 코드 대신에 Global 역할로 공용변수를 수정하는 코드를 넣었습니다.
+# ---------------------------------------------------------------------
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+  return render_template('index.html')
+
+def word_name_get(app, word):
+  app.word_list[0] = word
+
+@app.route('/result', methods=['POST'])
+def result():
+  if request.method == 'POST':
+    if request.form['word_type'] == 'amazement':
+      word = 'Amazement'
+      word_name_get(app, word)
+      #haptic = haptic_function()  # 감정 단어에 맞게 햅틱 함수를 다르게 하면 될듯. haptic_dynamic, haptic_sad 이런식으로...
+    elif request.form['word_type'] == 'tenderness':
+      word = 'Tenderness'
+      word_name_get(app, word)
+      #haptic = haptic_function()
+    elif request.form['word_type'] == 'nostalgia':
+      word = 'Nostalgia'
+      word_name_get(app, word)
+      #haptic = haptic_function()
+    elif request.form['word_type'] == 'calmness':
+      word = 'Calmness'
+      word_name_get(app, word)
+      #haptic = haptic_function()
+    elif request.form['word_type'] == 'power':
+      word = 'Power'
+      word_name_get(app, word)
+      #haptic = haptic_function()
+    elif request.form['word_type'] == 'joyful':
+      word = 'Joyful'
+      word_name_get(app, word)
+      #haptic = haptic_function()
+    elif request.form['word_type'] == 'tension':
+      word = 'Tension'
+      word_name_get(app, word)
+      #haptic = haptic_function()
+    elif request.form['word_type'] == 'sadness':
+      word = 'Sadness'
+      word_name_get(app, word)
+      #haptic = haptic_function()
+    else:
+      pass
+  elif request.method == 'GET':
+    pass
+  return render_template('result.html', word=word, haptic=None)
+
+def app_function(word_list):    # multiprocessing 실행을 위해 임의로 설정했습니다.
+    app.word_list = word_list
+    app.run()
+
+# ---------------------------------------------------------------------
+
+
+# Haptic 디바이스 제어 코드입니다. 상단 코드와 동시에 processing으로 실행이 됩니다.
+
+global Play, Music_index, Flag1_MusicReset
+
+def Emotion_Music_selecter(Music_index):    # Html에서 선택한 감정 단어에 맞는 haptic pattern을 찾아주는 함수입니다.
+    global Flag1_MusicReset                 # 추가로 감정이 변경되었다는 신호도 체크하고 있습니다.
+    if word_list[0] != "None":
+        index = np.where(Emotion_array == word_list[0])[0][0]
+        if index != Music_index:
+            Flag1_MusicReset = True
+        return index
+    else:
+        return -1
+
+def Flag_checking(Music_index):             # 음악 진행 도중에 감정을 다시 선택했는지 확인하는 코드입니다.
+    return Music_index != np.where(Emotion_array == word_list[0])[0][0]
+
+async def run(loop):        # haptic 디바이스 제어 코드입니다. async로 진행되어 디바이스 연결부터 전부 하나의 코드로 만들어져야 합니다.
+    global Play, Music_index, Flag1_MusicReset
+    Play = False
+    Music_index = -1
+    Flag1_MusicReset = False
+    fps = 10
+    haptic_array = np.load("haptic_array.npy")
+
+    #-------------------Default Setting 코드가 의도대로 진행 되도록 설정한 값이니 수정을 안하는게 좋습니다.
+
+    buzz_addr = "E5:72:19:73:4A:3B"
+    devices = await discover()
+    for d in devices:
+        if str(d).find("Buzz") > 0:
+            print("Found a Buzz! " + str(d) + "\r\nAddress substring: " + str(d)[:17])
+            # set the address to a found Buzz
+            buzz_addr = str(d)[:17]
+    async with BleakClient(buzz_addr, loop=loop) as client:
+        my_buzz = NeoDevice(client)
+        await asyncio.sleep(1)
+        x = await client.is_connected()
+        await asyncio.sleep(1)
+        await my_buzz.request_developer_authorization()
+        await my_buzz.accept_developer_api_terms()
+        await my_buzz.pause_device_algorithm()
+        print("Bluetooth connected")
+    #---------------------Bluetooth conncection
+        try:
+            while True:
+                if Play == False:       # 음악 선택 단계임을 보여주는 코드입니다.
+                    print("-----------------")
+                    print("-----------------")
+                    print("Music Select Mode")
+                    print("-----------------")
+                    print("-----------------")# 첫 코드를 실행할 경우 이 print가 실행된 이후에 음악을 골라야 원할한 진행이 됩니다.
+                    while True:               # 음악이 선택되기 전까지는 이 loop에서 선택을 기다립니다.
+                        Music_index = Emotion_Music_selecter(Music_index)
+                        if Flag1_MusicReset == True:    # 음악이 선택되면 flag를 통해 인식되면
+                            print("Music selected")     # 기본적인 parameter를 설정하고 loop를 빠져나갑니다.
+                            Flag1_MusicReset = False
+                            arr_buzz = haptic_array[Music_index]
+                            print("CHeck1", arr_buzz)
+                            Play = True
+                            break
+                    time.sleep(0.01)
+                elif Play == True:      # 음악 실행 코드입니다.
+                    print("Music Play Mode")
+                    buzz_vibration_frame = [0, 0, 0, 0]
+                    delay = -0.01  # 디바이스의 출력에 딜레이가 있으면 값을 추가합니다. 디바이스 출력이 늦을 수록 -값을 증가(단위: sec)
+                    length = arr_buzz.shape[0]
+                    Fin_check = False       # 디바이스가 종료되야하는지 체크하는 tool 입니다.
+                    # -------------------------Initial Setting
+                    pygame.mixer.init(44100, -16, 2, 10240)
+                    pygame.mixer.music.load(Music_array[Music_index])
+                    pygame.mixer.music.play()         # >> 음악 출력 코드입니다.
+                    start = time.time() + delay
+                    a = 0
+                    while True:     # 디바이스 실행이 진행되는 코드입니다.
+                            await asyncio.sleep(0.01)
+                            await my_buzz.vibrate_motors(buzz_vibration_frame)  # loop 중 디바이스의 출력값을 받는 부분입니다.
+                            if Fin_check:
+                                print("Music Finish")
+                                Play = False
+                                pygame.mixer.music.stop()  # 혹시 음악이 종료되지 않을 경우, 강제로 종료합니다.
+                                break
+                            a = int((time.time() - start) * fps)
+                            if a > length-1:    # 음악이 모두 재생되었음을 확인하는 부분입니다.
+                                Fin_check = True
+                            elif Flag_checking(Music_index):    # 음악을 새롭게 선택했음을 확인하는 부분입니다.
+                                Flag1_MusicReset = True
+                                Play = False
+                                Fin_check = True
+                                a += 9000000    # 강제로 Timeout시키는 방식으로 종료 >> 그래야 buzz 출력을 0으로 설정 가능
+                            if a > length-1:       # 디바이스의 종료 여부를 확인하고 마지막 출력이 0이 되도록 설정합니다.
+                                buzz_vibration_frame = [0, 0, 0, 0]
+                            else:
+                                buzz_vibration_frame = [arr_buzz[a], arr_buzz[a], arr_buzz[a], arr_buzz[a]]
+
+                            print("time:", time.time() - start, "OUTPUT:, ", buzz_vibration_frame)
+        except KeyboardInterrupt:
+                await my_buzz.resume_device_algorithm()
+                pass
+
+if __name__ == "__main__":
+    manager = Manager()
+    word_list = manager.list()
+    word_list.append("None")
+    proc = Process(target=app_function, args=(word_list,))
+    proc.start()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(loop))
+    proc.join()
